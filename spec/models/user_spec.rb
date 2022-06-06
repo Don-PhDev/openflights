61e30d2c77a50d4562aaf93610017c51c05f5b53
Don Forrest <forrest@philippinedev.com>
2022-06-06 22:13:49 +0800

    Add validation test on model User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,29 @@ RSpec.describe User, type: :model do
   describe "relationships" do
     it { should have_many(:reviews) }
   end
+
+  subject {
+    described_class.new(
+      email: "example@gmail.com",
+      password: "password"
+    )
+  }
+
+  describe "validations" do
+    it { should validate_uniqueness_of(:email) }
+
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without an email" do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a password" do
+      subject.password = nil
+      expect(subject).to_not be_valid
+    end
+  end
 end
