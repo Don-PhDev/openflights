4ae2ffd2f61d9ae90626e2027b88085939736012
Don Forrest <forrest@philippinedev.com>
2022-06-06 22:14:54 +0800

    Add association test on model Review

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,4 +1,8 @@
 require "rails_helper"
 
 RSpec.describe Review, type: :model do
+  describe "relationships" do
+    it { should belong_to(:airline) }
+    it { should belong_to(:user) }
+  end
 end
